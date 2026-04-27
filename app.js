8d4a9a77aa0bb418bb6a2243df1db2106e5c07cf
Yuvraj Singh <ys792608@gmail.com>
2026-04-27 20:56:02 +0530

    Update div background color on button click
    Change the background color of the div to a random color on button click.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ btn.addEventListener("click", function(){
     let h3 = document.querySelector("h3");
     let randomColor = getRandomColor();
     h3.innerText = randomColor;
+    
 
     let div = document.querySelector("div");
     div.style.backgroundColor = randomColor;    
